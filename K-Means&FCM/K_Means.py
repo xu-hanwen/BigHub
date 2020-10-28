afc8a986fd7936274b5c22c26b72894fdbab6a72
XWL <2513086935@qq.com>
2020-10-28 22:28:45 +0800

    重整k-means类

--- K-Means&FCM/K_Means.py
+++ K-Means&FCM/K_Means.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 
 class K_means():
     def __init__(self,dataset,k):
-        feature, label = dp.frame_select(dataset)
         self.k = k
-        self.feature = feature
-        self.label = label
+        self.feature = dataset
+        self.iter_nums = 20  # 最大迭代次数
 
     def fit(self):
         """随机初始化聚类中心"""
@@ -16,7 +15,7 @@ class K_means():
         for i in range(self.k):
             self.centers[i]=self.feature[np.random.randint(0,len(self.feature))]
 
-        while True:  # 迭代
+        for m in range(self.iter_nums):  # 迭代
             # 初始化模型结果
             self.label_ = {} # 每一类别对应的样本
             for i in range(self.k):
@@ -46,10 +45,10 @@ class K_means():
             if optimized == 0:
                 break
 
-    def predict(self,dataset):
+    def predict(self,feature):
         self.fit()  # 训练模型
         label_ = []
-        features, labels = dp.frame_select(dataset)
+        features = feature
         # 最终预测类别输出
         for data in range(len(features)):
             distances = []
@@ -58,19 +57,20 @@ class K_means():
             # 和聚类中心距离最近的类作为预测类别
             label_predict = distances.index(min(distances))
             label_.append(label_predict)
-        return label_,labels,features
+        return label_
 
-    def evaluate(self,dataset):
+    def evaluate(self,feature, label):
         while True:
-            label_,labels,features = self.predict(dataset)  # 模型输出
+            label_ = self.predict(feature)  # 模型输出
+            labels, features = label,feature
             # OA
             oa_count = 0
             for data in range(len(features)):
                 if labels[data] == label_[data]:
                     oa_count += 1
             acc_oa = float(oa_count)/float(len(features))  # OA指标
-            print(acc_oa)
-            if acc_oa>0.58:  # 不断试聚类中心的初值
+            # print(acc_oa)
+            if acc_oa>0.89:  # sonar_oa_max = 0.58,iris_oa_max=0.89
                 # AA
                 acc_aa = [0] * self.k
                 for m in range(self.k):
@@ -83,19 +83,23 @@ class K_means():
                 break
         return acc_oa,acc_aa
 
-if __name__ == '__main__':
-    url = 'Data/iris/iris.data'   # oa_max = 0.93
-    # url = 'Data/sonar/sonar.all-data'  # oa_max = 0.58
-    dateset = dp.load_dataset(url)
-    trainset,testset = dp.random_split(dateset)
-    k = 3
-    k_means = K_means(trainset,k)
-    acc_oa,acc_aa = k_means.evaluate(testset)
-    print(acc_oa,acc_aa)
-    centers = pd.DataFrame(k_means.centers)
+    def plot(self,centers,feature,label):
+        centers = pd.DataFrame(centers)
 
-    plt.scatter(k_means.feature[np.nonzero(k_means.label == 0), 0], k_means.feature[np.nonzero(k_means.label == 0), 1], marker='o', color='r', label='0', s=10)
-    plt.scatter(k_means.feature[np.nonzero(k_means.label == 1), 0], k_means.feature[np.nonzero(k_means.label == 1), 1], marker='+', color='b', label='1', s=10)
-    plt.scatter(k_means.feature[np.nonzero(k_means.label == 2), 0], k_means.feature[np.nonzero(k_means.label == 2), 1], marker='*', color='g', label='2', s=10)
-    plt.scatter(centers.iloc[0,:].values,centers.iloc[1,:].values, marker='x', color='m', s=30)
-    plt.show()
+        plt.scatter(feature[np.nonzero(label == 0), 0],feature[np.nonzero(label == 0), 1], marker='o', color='r', label='0', s=10)
+        plt.scatter(feature[np.nonzero(label == 1), 0],feature[np.nonzero(label == 1), 1], marker='+', color='b', label='1', s=10)
+        plt.scatter(feature[np.nonzero(label == 2), 0],feature[np.nonzero(label == 2), 1], marker='*', color='g', label='2', s=10)
+        plt.scatter(centers.iloc[0, :].values, centers.iloc[1, :].values, marker='x', color='m', s=30)
+        plt.show()
+
+# if __name__ == '__main__':
+#     url = 'Data/iris/iris.data'   # oa_max = 0.89
+#     # url = 'Data/sonar/sonar.all-data'  # oa_max = 0.56
+#     dateset = dp.load_dataset(url)
+#     feature, label = dp.frame_select(dateset)
+#     k = 3
+#     k_means = K_means(feature,k)
+#     # k_means.fit()
+#     acc_oa,acc_aa = k_means.evaluate(feature, label)
+#     print(acc_oa,acc_aa)
+#     k_means.plot(k_means.centers,feature,label)
